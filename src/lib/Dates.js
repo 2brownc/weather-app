700ff72654c137ff5dfab3f7af57ab917709ec74
brown <86018533+2brownc@users.noreply.github.com>
2022-07-19 20:57:29 +0530

    a way to display local date from UTC / openweathermap

--- src/lib/Dates.js
+++ src/lib/Dates.js
@@ -5,6 +5,7 @@ function getMonthDateFromNow(days) {
   const year = currentDate.getUTCFullYear();
 
   const requiredDate = new Date(
+    // multiply with 1000 because that's how openweathermap issues dates
     Date.UTC(year, month, date) + days * 60 * 60 * 24 * 1000,
   );
 
@@ -16,4 +17,16 @@ function getMonthDateFromNow(days) {
   return requiredDate.toLocaleDateString(undefined, localDateOptions);
 }
 
-export default getMonthDateFromNow;
+function getLocalDateFromUTC(utc) {
+  // multiply with 1000 because that's how openweathermap issues dates
+  const date = new Date(utc * 1000);
+  const localDateOptions = {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  };
+
+  return date.toLocaleDateString(undefined, localDateOptions);
+}
+
+export { getMonthDateFromNow, getLocalDateFromUTC };
